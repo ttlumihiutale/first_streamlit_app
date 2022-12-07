248754b488b74793c9b6a6e08c70aae1548e63bc
ttlumihiutale <120026611+ttlumihiutale@users.noreply.github.com>
2022-12-07 15:01:57 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,10 @@
 import streamlit
-streamlit.title("Teemu testaa")
+streamlit.title("My Parents New Healthy Diner")
+
+streamlit.header('Breakfast Favorites')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
