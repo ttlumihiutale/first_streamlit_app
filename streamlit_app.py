5a63b1d138871b29c9dce41ab04eb440c7f71452
ttlumihiutale <120026611+ttlumihiutale@users.noreply.github.com>
2022-12-12 09:03:34 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,9 @@
 import streamlit
 import pandas
 import requests
+
+streamlit.stop()
+
 import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
