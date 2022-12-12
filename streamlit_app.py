d8a0547bc635c8af74bb76b342870f3b4e8f3a5c
ttlumihiutale <120026611+ttlumihiutale@users.noreply.github.com>
2022-12-12 09:19:01 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,8 +6,8 @@ from urllib.error import URLError
 
 #streamlit.stop()
 
-def get_fruityvice_data():
-  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+def get_fruityvice_data(this_fruit_choice):
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
   return fruityvice_normalized
   
